5444a8279dfc078e4807f88a185e5296d6fbd1d2
tony <bluein007@gmail.com>
2025-10-08 03:23:54 +0900

    Front Version 0.0.8

--- src/prj/prjMain.js
+++ src/prj/prjMain.js
@@ -27,7 +27,7 @@ const ___prj = {
     //----------------------------------------------------------
     name: 'CODE',
     framework: 'SvelteKit 5.0.1',
-    version: '0.0.7',
+    version: '0.0.8',
     author: 'bluein007',
     authorEmail: 'bluein007@gmail.com',
 
